cea2eb84ebb86bba7a6c7591be0096c02814c57e
fahmiin <fahmiin@seedly.sg>
2022-07-09 02:10:13 +0800

    feat: added history section

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,9 @@
 
         <!-- Forecasting tab -->
         <v-tab-item>
+          <p class="mt-3">This mode predicts the weather pattern for the next 7 days for a given location.</p>
           <!-- Query fields -->
-          <div class="d-flex align-center mt-5">
+          <div class="d-flex align-center mt-3">
             <v-select
               v-model="location"
               :items="locations"
@@ -23,83 +24,139 @@
             <v-btn
               color="success"
               depressed
-              class="ml-3"
+              class="ml-sm-3"
               :loading="forecastLoading"
               @click="forecast()"
             >Predict!</v-btn>
           </div>
-
-          <!-- Display weather stats -->
-          <v-row
-            v-if="items.length > 0"
-            class="my-5"
-          >
-            <v-col
-              v-for="(item, index) in items" :key="index"
-              md="3"
-            >
-              <v-card class="mx-1">
-                <v-list-item two-line>
-                  <v-list-item-content>
-                    <v-list-item-title class="text-h5">
-                      {{ item.conditions }}
-                    </v-list-item-title>
-                    <v-list-item-subtitle>{{ item.datetime }}, {{ item.description }}</v-list-item-subtitle>
-                  </v-list-item-content>
-                </v-list-item>
-
-                <v-card-text>
-                  <v-row align="center">
-                    <v-col
-                      class="text-h4"
-                      cols="8"
-                    >
-                      {{ item.temp }}&deg;F
-                    </v-col>
-                    <v-col cols="4">
-                      <v-img
-                        :src="`https://cdn.vuetifyjs.com/images/cards/${'sun'}.png`"
-                        width="92"
-                      ></v-img>
-                    </v-col>
-                  </v-row>
-                </v-card-text>
-
-                <v-divider></v-divider>
-
-                <v-list-item>
-                  <v-list-item-icon>
-                    <v-icon>mdi-send</v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-subtitle>{{ item.windspeed }} mph</v-list-item-subtitle>
-                </v-list-item>
-
-                <v-divider></v-divider>
-
-                <v-list-item>
-                  <v-list-item-icon>
-                    <v-icon>mdi-cloud</v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-subtitle>{{ item.cloudcover }}% cloud cover</v-list-item-subtitle>
-                </v-list-item>
-              </v-card>
-            </v-col>
-          </v-row>
-          <p
-            v-else
-            class="text-center my-8"
-          >
-            No data yet. Select a location to start!
-          </p>
         </v-tab-item>
 
         <!-- History tab -->
         <v-tab-item>
-
+          <p class="mt-3">This mode lets you search trough weather history between 2 dates.</p>
+          <!-- Query fields -->
+          <div class="d-flex align-center mt-3">
+            <v-select
+              v-model="location"
+              :items="locations"
+              outlined
+              dense
+              hide-details
+              label="Choose a location"
+            ></v-select>
+            <v-menu>
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                  v-model="startDate"
+                  label="Choose Start Date"
+                  outlined
+                  dense
+                  hide-details
+                  class="ml-sm-3"
+                  v-bind="attrs"
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker
+                v-model="startDate"
+                no-title
+              ></v-date-picker>
+            </v-menu>
+            <v-menu>
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                  v-model="endDate"
+                  label="Choose End Date"
+                  outlined
+                  dense
+                  hide-details
+                  class="ml-sm-3"
+                  v-bind="attrs"
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker
+                v-model="endDate"
+                no-title
+              ></v-date-picker>
+            </v-menu>
+            <v-btn
+              color="success"
+              depressed
+              class="ml-sm-3"
+              :loading="historyLoading"
+              @click="history()"
+            >Predict!</v-btn>
+          </div>
         </v-tab-item>
       </v-tabs>
 
+      <!-- Display weather stats -->
+      <v-row
+        v-if="items.length > 0"
+        class="my-5"
+        :class="{ 'side-scroll': $vuetify.breakpoint.smAndUp }"
+      >
+        <v-col
+          v-for="(item, index) in items" :key="index"
+          md="3"
+        >
+          <v-card class="mx-1">
+            <v-list-item two-line>
+              <v-list-item-content>
+                <v-list-item-title class="text-h5 mb-3">
+                  {{ item.datetime }}
+                </v-list-item-title>
+                <v-list-item-subtitle class="mb-2">{{ item.conditions }}</v-list-item-subtitle>
+                <small>{{ item.description }}</small>
+              </v-list-item-content>
+            </v-list-item>
 
+            <v-divider></v-divider>
+
+            <v-card-text>
+              <v-row align="center">
+                <v-col
+                  class="text-h4"
+                  cols="8"
+                >
+                  {{ item.temp }}&deg;F
+                </v-col>
+                <v-col cols="4">
+                  <v-img
+                    :src="`https://cdn.vuetifyjs.com/images/cards/${'sun'}.png`"
+                    width="92"
+                  ></v-img>
+                </v-col>
+              </v-row>
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-weather-windy</v-icon>
+              </v-list-item-icon>
+              <v-list-item-subtitle>{{ item.windspeed }} mph</v-list-item-subtitle>
+            </v-list-item>
+
+            <v-divider></v-divider>
+
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-cloud</v-icon>
+              </v-list-item-icon>
+              <v-list-item-subtitle>{{ item.cloudcover }}% cloud cover</v-list-item-subtitle>
+            </v-list-item>
+          </v-card>
+        </v-col>
+      </v-row>
+      <p
+        v-else
+        class="text-center my-8"
+      >
+        No data yet. Select a location to start!
+      </p>
     </v-main>
   </v-app>
 </template>
@@ -110,18 +167,35 @@ import axios from 'axios'
 export default {
   name: 'App',
   data: () => ({
-    location: '',
+    location: null,
+    startDate: null,
+    endDate: null,
     items: [],
     locations: ['Brunei', 'Australia', 'India', 'Japan', 'China', 'Malaysia', 'Indonesia'],
     forecastLoading: false,
+    historyLoading: false
   }),
   methods: {
     async forecast() {
       this.forecastLoading = true
-      await axios.get('http://localhost:3000/forecast', { params: { location: this.location } }).then(res => {
+      await axios.get(`${process.env.VUE_APP_API_URL}/forecast`, { params: { location: this.location } }).then(res => {
         const { data } = res
         this.items = data.data.days
         this.forecastLoading = false
+      }).catch(() => {})
+    },
+    async history() {
+      this.historyLoading = true
+      await axios.get(`${process.env.VUE_APP_API_URL}/history`, {
+        params: {
+          location: this.location,
+          start_date: this.startDate,
+          end_date: this.endDate
+        }
+      }).then(res => {
+        const { data } = res
+        this.items = data.data.days
+        this.historyLoading = false
       }).catch(() => {})
     }
   }
@@ -129,7 +203,7 @@ export default {
 </script>
 
 <style lang="scss">
-  .row {
+  .side-scroll {
     flex-wrap: nowrap !important;
     overflow: auto !important;
   }
