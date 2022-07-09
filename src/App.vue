987c341b35481576297ed52bdfd8a6e987c4bfd2
fahmiin <fahmiin@seedly.sg>
2022-07-10 00:26:58 +0800

    chore: added weather card component

--- src/App.vue
+++ src/App.vue
@@ -132,63 +132,7 @@
           v-for="(item, index) in items" :key="index"
           md="3"
         >
-          <v-card class="mx-1">
-            <v-list-item two-line>
-              <v-list-item-content>
-                <v-list-item-title class="text-h5 mb-3">
-                  {{ item.datetime }}
-                </v-list-item-title>
-                <v-list-item-subtitle class="mb-2">{{ item.conditions }}</v-list-item-subtitle>
-                <small>{{ item.description }}</small>
-              </v-list-item-content>
-            </v-list-item>
-
-            <v-divider></v-divider>
-
-            <v-card-text>
-              <v-row align="center">
-                <v-col
-                  class="text-h4"
-                  cols="8"
-                >
-                  {{ item.temp }}&deg;F
-                </v-col>
-                <v-col cols="4">
-                  <v-img
-                    :src="require(`@/assets/${item.icon}.png`)"
-                    width="92"
-                  ></v-img>
-                </v-col>
-              </v-row>
-            </v-card-text>
-
-            <v-divider></v-divider>
-
-            <v-list-item>
-              <v-list-item-icon>
-                <v-icon>mdi-weather-windy</v-icon>
-              </v-list-item-icon>
-              <v-list-item-subtitle>{{ item.windspeed }} mph</v-list-item-subtitle>
-            </v-list-item>
-
-            <v-divider></v-divider>
-
-            <v-list-item>
-              <v-list-item-icon>
-                <v-icon>mdi-cloud</v-icon>
-              </v-list-item-icon>
-              <v-list-item-subtitle>{{ item.cloudcover }}% cloud cover</v-list-item-subtitle>
-            </v-list-item>
-
-            <v-divider></v-divider>
-
-            <v-list-item>
-              <v-list-item-icon>
-                <v-icon>mdi-weather-rainy</v-icon>
-              </v-list-item-icon>
-              <v-list-item-subtitle>{{ item.precipprob }}% will rain</v-list-item-subtitle>
-            </v-list-item>
-          </v-card>
+          <weather-card :weatherStats="item"></weather-card>
         </v-col>
       </v-row>
       <p
@@ -203,9 +147,11 @@
 
 <script>
 import axios from 'axios'
+import { WeatherCard } from '@/components'
 
 export default {
   name: 'App',
+  components: { WeatherCard },
   data: () => ({
     location: null,
     startDate: null,
---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-card class="mx-1">
+    <v-list-item two-line>
+      <v-list-item-content>
+        <v-list-item-title class="text-h5 mb-3">
+          {{ weatherStats.datetime }}
+        </v-list-item-title>
+        <v-list-item-subtitle class="mb-2">{{ weatherStats.conditions }}</v-list-item-subtitle>
+        <small>{{ weatherStats.description }}</small>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-card-text>
+      <v-row align="center">
+        <v-col
+          class="text-h4"
+          cols="8"
+        >
+          {{ weatherStats.temp }}&deg;F
+        </v-col>
+        <v-col cols="4">
+          <v-img
+            :src="require(`@/assets/${weatherStats.icon}.png`)"
+            width="92"
+          ></v-img>
+        </v-col>
+      </v-row>
+    </v-card-text>
+
+    <v-divider></v-divider>
+
+    <v-list-item>
+      <v-list-item-icon>
+        <v-icon>mdi-weather-windy</v-icon>
+      </v-list-item-icon>
+      <v-list-item-subtitle>{{ weatherStats.windspeed }} mph</v-list-item-subtitle>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list-item>
+      <v-list-item-icon>
+        <v-icon>mdi-cloud</v-icon>
+      </v-list-item-icon>
+      <v-list-item-subtitle>{{ weatherStats.cloudcover }}% cloud cover</v-list-item-subtitle>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list-item>
+      <v-list-item-icon>
+        <v-icon>mdi-weather-rainy</v-icon>
+      </v-list-item-icon>
+      <v-list-item-subtitle>{{ weatherStats.precipprob }}% will rain</v-list-item-subtitle>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    weatherStats: Object
+  },
+}
+</script>
